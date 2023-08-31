0ee6f12fd79d8da53ef0e3e10782ca324ec0f757
Alex <115927556+Renchaku@users.noreply.github.com>
2023-09-01 00:04:25 +0100

    Add files via upload
    For now, you will have to manually put it under .minecraft/config/chattriggers/modules

---
+++ mibore/index.js
@@ -0,0 +1,5 @@
+register("chat", (message, event) => {
+    ChatLib.chat("&r&a&r&6Mining Speed Boost &r&ais now available! &r&a&r&6Mining Speed Boost &r&ais now available!\n&r&a&r&6Mining Speed Boost &r&ais now available! &r&a&r&6Mining Speed Boost &r&ais now available!\n&r&a&r&6Mining Speed Boost &r&ais now available! &r&a&r&6Mining Speed Boost &r&ais now available!\n&r&a&r&6Mining Speed Boost &r&ais now available! &r&a&r&6Mining Speed Boost &r&ais now available!\n&r&a&r&6Mining Speed Boost &r&ais now available! &r&a&r&6Mining Speed Boost &r&ais now available!\n");
+  }).setChatCriteria("&r&a&r&6Mining Speed Boost &r&ais now available!").setContains();
+  
+  
